d79ee0a35b4dee14593f9b5baa372389918f946e
Atze <atzeus@gmail.com>
2020-03-16 14:42:08 +0100

    Adds exercises

---
+++ src/warmup/Exercises.scala
@@ -0,0 +1,281 @@
+package warmup
+
+import org.scalacheck.Prop._
+import org.scalacheck.Prop
+import org.scalacheck.Properties
+
+import scala.collection.mutable
+import scala.collection.mutable.Map
+
+object Exercises {
+
+
+  /* assignment 1:
+
+      calculate the _index_ of the first of occurrence of the maximum of an array, give -1 if the array is empty
+      (there is no index of the maximum)
+
+      examples:
+        indexOfMax(Array(6,5,4,3,6)) == 0
+        indexOfMax(Array(4,5,6,3,6)) == 2
+        indexOfMax(Array()) == -1
+
+   */
+  def indexOfMax(a : Array[Int]) : Int = {
+   return -1
+  }
+
+
+
+  /* Assignment 2:
+
+  calculate the average exam score from a string containing a list of grades. A grade is either a number in the 1-10
+  range or the string "NS" for no show. No show grades have no effect on the average grade.
+
+  examples:
+
+  averageGrade("NS NS 9 NS NS 5") == 7
+  averageGrade("8 8.2 NS NS 9 4 1 5.5") == 5.95
+
+   */
+  def averageGrade(grades : String) = {
+    0
+  }
+
+    /* Assignment 3:
+
+      An infamous unsolved problem in discrete math is the
+      "Collatz conjecture" (https://en.wikipedia.org/wiki/Collatz_conjecture).
+
+
+      For any positive integer n, the corresponding "Collatz sequence" starts with the number n.
+      The next number in the sequence is defined as follows:
+          if n is even, the next number is n/2
+          if n is odd,  the next number is 3n + 1
+      The subsequencent numbers are constructed in the same way.
+      For example, if we start with 11, the corresponding Collatz sequence is:
+      11,34,17,52,26,13,40,20,10,5,16,8,4,2,1,4,2,1,4,...
+      Once a Collatz sequence has reached 1, the sequence will repeat the pattern 4,2,1 forever.
+
+      The Collatz conjecture states that, no matter which integer we start with, the collatz sequence will
+      eventually reach 1 and repeat the pattern 4,2,1 forever. The conjecture is probably true, and has been checked
+      by computers for all starting values up to 87×2^60(!). However, there is no mathematical proof for seemingly simple
+      statement. There have been  mathematicians that have spent years of continued study on the conjecture, without
+      success.Mathematical professor Jeffrey Lagarias has written a book about Collatz conjecture, and stated that
+       "this is an extraordinarily difficult problem, completely out of reach of present day mathematics".
+
+      Luckily, programming the Collatz sequence is much easier! Please implement a function which when given a starting
+       number, returns the number of elements in the sequence before reaching 1. For example:
+       collatzLength(11) = 14
+       collatzLength(1) = 0
+       collatzLength(4) = 2
+       collatzLength(7) = 16
+       collatzLength(27) = 111
+     */
+
+
+
+  def collatzLength(start : Long) : Long = {
+   -1
+  }
+
+
+  /* Assignment 4: Write a function that produces "ASCII art" of a diamond of height n such the following diamond of height 7 :
+
+   #
+  ###
+ #####
+#######
+ #####
+  ###
+   #
+
+
+Diamond of height 12
+
+     #
+    ###
+   #####
+  #######
+ #########
+###########
+###########
+ #########
+  #######
+   #####
+    ###
+     #
+
+
+Note that the last character of each line should be # and that hence the line should not end in spaces.
+   */
+
+  def diamondString(height : Int) : String = {
+   ""
+  }
+
+
+  /* Assignment 5: Implement a function word count that gives the word counts for all words in a string. Ignore whitespace and capitalization.
+
+
+
+   Examples:
+    "the monkey hugs the monkey" gives Map(the -> 2, monkey -> 2, hugs -> 1)
+
+  "A horse, a horse! My kingdom for a horse!" gives Map(for -> 1, a -> 3, my -> 1, horse -> 3, kingdom -> 1)
+
+
+  "A woman is but a woman. A man is but a man. A child is but a child."
+
+  gives
+
+  Map(is -> 3, but -> 3, man -> 2, child -> 2, woman -> 2, A -> 3, a -> 3)
+
+   */
+
+  def wordCount(text : String) : mutable.Map[String,Int] = Map()
+
+
+
+  /* Assignment 6: Elo ratings
+
+  In chess and various other form of competition the skill of a player is indicated using
+  the Elo-rating system. The elo-rating is a number in the 0-3000 range, where 0 is an absolute beginner and
+  2900 is the world champion.
+
+  When given the ratings of two players rA and rB, the probability of a winning the game is calculated as follows:
+
+  eA = 1 / ( 1 + 10^((rB - ra) / 400)))
+
+  This gives a number between 0 (100 % sure that a loses) and 1 (100 % sure that a wins).
+
+  After playing a game or tournament, the elo ratings of the player are updated based on their wins and loses. The
+  new rating of a player is calculated based on the probability of a winning eA and the actual outcome aA.
+
+  rA' = rA + k * (aA - eA)
+
+  where k is a constant controlling the importance of the game (typically between 16-32). We use k=24.
+
+  For example, if player a has an elo rating of 2000 and player b has a elo rating of 2200, then the probability of
+  player a winning is 1 / ( 1 + 10^((2200 - 2000) / 400))) = 0.24. Suppose player a wins the game, so aA=1.0.
+  Then the new elo score of player a is 2000 + 24 * (1.0 - 0.24) = 2018.24. For player b the computation is reversed,
+  his probability of winning was (1 - 0.24) = 0.76. The new elo score of player b is
+  2200 + 24 * (0.0 - 0.76) = 2181.76
+
+  During a tournament, the rating of players is not changed until the tournament finishes, after which all
+  games are processed and everyone's new elo score is determined.
+
+  Program a method which updates the elo ratings of all players. You can (and should) add method to classes below.
+
+
+  We test this as follows:
+
+    val jaap = new Player("Jaap",2000)
+    val piet = new Player("Piet", 2200)
+    val game = new Game(jaap,piet,0.0)
+    Exercises.updateEloScores(List(jaap,piet), List(game) )
+    assert(jaap.rating === 2018.24 +- 0.01)
+    assert(piet.rating === 2181.76 +- 0.01)
+
+   */
+
+  val eloK = 24
+
+  def updateEloScores(players : List[Player] , games : List[Game]) = {
+  }
+
+  class Player(
+                val name : String,
+                var rating : Double) {
+  }
+
+  class Game(
+              val playerA : Player,
+              val playerB : Player,
+              val outcome : Double, // 0 means playerA won, 1 means playerB won, 0.5 means draw
+            )
+  {
+  }
+
+  /* Assignment 7: Program a method that split any non-empty array into two arrays.
+
+  If the input array has length l , the first array should have length (l / 2),
+  the second array should have length (l - (l / 2)). For example, for
+  length 7, the first array has length 3, while the second has length 4.
+
+  If we put the second resulting array behind the first one, we should get the original array back.
+
+  example: [5,3,10,5,6,1,3] -> ([5,3,10],[5,6,1,3])
+
+   */
+  def splitArray(a : Array[Int]) : (Array[Int],Array[Int]) = {
+    return null
+  }
+
+
+  /* Assignment 8:
+
+     Program a method that when given two sorted arrays a and b, returns a new sorted array c that has the elements from
+     array and array b. For example
+     when given
+     a = [1,3,5,6,10]
+     b = [1,4,6,8]
+
+     the resulting array should be:
+
+     c = [1,1,3,4,5,6,6,9,10]
+
+     This method should not call a sorting method. Instead the resulting array should be produced by "zipping" the
+     two input arrays together. We repeatedly select the least element that we did not consider before from a and b
+     and include this in c.
+
+     For example:
+
+     a = [1,3,5,6,10]
+              ^
+     b = [1,4,6,8]
+            ^
+     c = [1,1,3,...]
+      the arrows (^) point to the lowest element we did not consider before. Of these, the element 4 from b is less
+      then the element 5 from a. For this reason, we select 4 as the next element and advance the arrow ^ for b to point
+      to 6.
+
+   */
+  def mergeSortedArrays(a : Array[Int], b : Array[Int]) : Array[Int] = {
+    return null
+  }
+
+
+
+
+
+  /* Assignment 9 : Mergesort
+
+  Implement mergesort. Mergesort is a recursive sorting algorithm that works by splitting arrays and merging them.
+  More precisely, mergeSort works as follows:
+
+   If the input array is of length <= 1, then the array is already sorted an the input array is returned.
+   If the input array is of length >= 2, then the input array is split in two (using the splitArray function),
+   the two smaller arrays are sorted using mergesort, and the resulting two sorted arrays are merged using
+   mergeSortedArrays.
+
+   Example:
+        7 5 6 1    -- split
+       /      \     -- merge sort left and right recursively
+     7 5     6 1     -- split both sides
+     /  \    /  \
+    7   5   6   1     -- merge both sides
+    \   /   \  /    -- left and right merge sort done
+    5 7     1 6    -- merge
+     \       /
+      1 5 6 7
+   */
+
+  def mergeSort(a : Array[Int]) : Array[Int] = {
+    null
+  }
+
+
+
+}
+
---
+++ test/Tests.scala
@@ -0,0 +1,262 @@
+package test
+
+
+
+import warmup.Exercises
+import warmup.Exercises.{Game, Player}
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatestplus.junit.JUnitRunner
+import org.scalatest.prop.Checkers
+import org.scalacheck.Arbitrary._
+import org.scalacheck.Prop._
+import org.scalatest.concurrent.TimeLimitedTests
+import org.scalatest.time.{Second, Seconds, Span}
+import org.scalactic.Tolerance._
+
+@RunWith(classOf[JUnitRunner])
+class Tests extends FunSuite with TimeLimitedTests {
+
+  test("max0") {
+    assert(Exercises.indexOfMax(Array())== -1)
+  }
+
+  test("max1") {
+    assert(Exercises.indexOfMax(Array(1))==0)
+  }
+
+  test("max2") {
+    assert(Exercises.indexOfMax(Array(1,2,3))==2)
+  }
+
+  test("max3") {
+    assert(Exercises.indexOfMax(Array(3,10,3,10))== 1)
+  }
+
+  test("max4") {
+    assert(Exercises.indexOfMax(Array(6,5,4,3,6)) == 0)
+  }
+
+  test("max5") {
+    assert(Exercises.indexOfMax(Array(4,5,6,3,6)) == 2)
+  }
+
+  test("testaverage0") {
+    assert(Exercises.averageGrade("NS NS 9 NS NS 5") == 7)
+  }
+
+  test("testaverage1") {
+    assert(Exercises.averageGrade("8 8.2 NS NS 9 4 1 5.5") == 5.95)
+  }
+
+  test("testaverage2") {
+    assert(Exercises.averageGrade("7 8 5.5 9.2 NS 7.8 5.4 NS NS 7 9.2") == 7.387499999999999)
+  }
+
+  //        collatzLength(11) = 14
+  //       collatzLength(1) = 0
+  //       collatzLength(4) = 2
+  //       collatzLength(7) = 16
+
+  test("collatz0") {
+    assert(Exercises.collatzLength(1) == 0)
+  }
+
+  test("collatz1") {
+    assert(Exercises.collatzLength(11) == 14)
+  }
+
+  test("collatz2") {
+    assert(Exercises.collatzLength(7) == 16)
+  }
+
+  test("collatz3") {
+    assert(Exercises.collatzLength(27) == 111)
+  }
+
+  test("collatz4") {
+    assert(Exercises.collatzLength(837799) == 524)
+  }
+
+  test("diamond3") {
+    assert(Exercises.diamondString(3) == diamond3)
+  }
+
+  test("diamond4") {
+    assert(Exercises.diamondString(4) == diamond4)
+  }
+
+  test("diamond7") {
+    assert(Exercises.diamondString(7) == diamond7)
+  }
+
+  test("diamond12") {
+    assert(Exercises.diamondString(12) == diamond12)
+  }
+
+  val diamond3 =
+    """ #
+       |###
+       | #""".stripMargin
+
+  val diamond4 =
+    """ #
+      |###
+      |###
+      | #""".stripMargin
+
+  val diamond7 =
+    """   #
+      |  ###
+      | #####
+      |#######
+      | #####
+      |  ###
+      |   #""".stripMargin
+
+  val diamond12 =
+    """     #
+      |    ###
+      |   #####
+      |  #######
+      | #########
+      |###########
+      |###########
+      | #########
+      |  #######
+      |   #####
+      |    ###
+      |     #""".stripMargin
+
+
+
+  test("wordcount0") {
+    assert(Exercises.wordCount("the monkey hugs the monkey") ==  Map("the" -> 2, "monkey" -> 2, "hugs" -> 1))
+  }
+
+  test("wordcount1") {
+    assert(Exercises.wordCount("A horse, a horse! My kingdom for a horse!") ==  Map("for" -> 1, "a" -> 3, "my" -> 1, "horse" -> 3, "kingdom" -> 1))
+  }
+
+
+  test("wordcount2") {
+    assert(Exercises.wordCount("A woman is but a woman. A man is but a man. A child is but a child.") ==  Map("is" -> 3, "but" -> 3, "a" -> 6, "man" -> 2, "child" -> 2, "woman" -> 2))
+
+  }
+
+  test("elo1") {
+    val jaap = new Player("Jaap",2000)
+    val piet = new Player("Piet", 2200)
+    val game = new Game(jaap,piet,0.0)
+    Exercises.updateEloScores(List(jaap,piet), List(game) )
+    assert(jaap.rating === 2018.24 +- 0.01)
+    assert(piet.rating === 2181.76 +- 0.01)
+  }
+
+
+  test("elo2") {
+    val piet = new Player("Piet", 1500)
+    val mies = new Player("Mies", 1700)
+    val ella = new Player("Ella", 1400)
+
+    val players = List(piet,mies,ella)
+
+    val game1 = new Game(mies,piet,0.0)
+    val game2 = new Game(ella, mies, 1)
+    val game3 = new Game(piet, ella,0)
+
+    val games = List(game1,game2,game3)
+
+    Exercises.updateEloScores(players, games)
+
+    assert(piet.rating === 1502.87 +- 0.01)
+    assert(mies.rating === 1709.39 +- 0.01)
+    assert(ella.rating === 1387.74 +- 0.01)
+  }
+
+  test("elo3") {
+    val jaap = new Player("Jaap",2000)
+    val piet = new Player("Piet", 2200)
+    val mies = new Player("Mies", 2600)
+    val ella = new Player("Ella", 1900)
+
+    val players = List(jaap,piet,mies,ella)
+
+    val game1 = new Game(jaap,piet,0.0)
+    val game2 = new Game(mies,piet,0.0)
+    val game3 = new Game(ella, mies, 0)
+    val game4 = new Game(ella, jaap, 1.0)
+    val game5 = new Game(piet, ella,0)
+    val game6 = new Game(mies,jaap,0)
+
+    val games = List(game1,game2,game3,game4,game5,game6)
+
+    Exercises.updateEloScores(players, games)
+
+    assert(jaap.rating === 2026.13 +- 0.01)
+    assert(piet.rating === 2183.20 +- 0.01)
+    assert(mies.rating === 2579.34 +- 0.01)
+    assert(ella.rating === 1911.31 +- 0.01)
+  }
+
+  def testSplit(a : Array[Int]) = {
+    val (l,r) = Exercises.splitArray(a)
+    assert(l.size == a.length / 2)
+    assert((l ++ r).sameElements(a))
+  }
+
+  test("split0") {
+    testSplit(Array(5,3,10,5,6,1,3))
+  }
+
+  test("split1") {
+    testSplit(Array(6,5,3,6,1,3))
+  }
+
+  test("split2" ) {
+    testSplit(Array(6,5,3,6,1,3,8,5,3))
+  }
+
+  test("merge0") {
+    val ar = Exercises.mergeSortedArrays(Array(1, 3, 5, 6, 10), Array(1, 4, 6, 8))
+    assert(ar.sameElements(Array(1,1,3,4,5,6,6,8,10)))
+  }
+
+  test("merge1") {
+    val ar = Exercises.mergeSortedArrays(Array(1, 3,5,7,9), Array(2,4,6,8))
+    assert(ar.sameElements(Array(1,2,3,4,5,6,7,8,9)))
+  }
+
+  test("merge2") {
+    val ar = Exercises.mergeSortedArrays(Array(1, 2,5,8,10), Array(2,2,6,8,9))
+    assert(ar.sameElements(Array(1,2,2,2,5,6,8,8,9,10)))
+  }
+
+  test("merge3") {
+    val ar = Exercises.mergeSortedArrays(Array(1, 3,5,10), Array(2,8))
+    assert(ar.sameElements(Array(1,2,3,5,8,10)))
+  }
+
+  test("mergesort0") {
+    val a = Array(6,5,3,1,8,7,2,4)
+    val b = Exercises.mergeSort(a)
+    assert(b.sameElements(Array(1,2,3,4,5,6,7,8)))
+  }
+
+  test("mergesort1") {
+    val a = Array(7,6,3,6,8,2,1,5,4)
+    val b = Exercises.mergeSort(a)
+    assert(b.sameElements(Array(1,2,3,4,5,6,6,7,8)))
+  }
+
+  test("mergesort2") {
+    val a = Array(8,7,6,5,4,3,2,1)
+    val b = Exercises.mergeSort(a)
+    assert(b.sameElements(Array(1,2,3,4,5,6,7,8)))
+  }
+
+
+
+  override def timeLimit: Span = Span(5,Seconds)
+}
