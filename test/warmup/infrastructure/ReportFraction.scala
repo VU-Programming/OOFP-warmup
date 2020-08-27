5ae64fe0a1697a5343b4328801e4b49a4ac163d5
Atze <atzeus@gmail.com>
2020-08-27 13:48:07 +0200

    Move depency to compile time to ensure it being downloaded

--- test/warmup/infrastructure/ReportFraction.scala
+++ test/warmup/infrastructure/ReportFraction.scala
@@ -6,11 +6,11 @@ import org.scalatest.{Args, ConfigMap, Reporter}
 import org.scalatest.events.{Event, TestFailed, TestSucceeded}
 import warmup.AllTests
 
-class CustumReporter extends Reporter {
+class CustumReporter(val out : PrintStream) extends Reporter {
     override def apply(event: Event): Unit = {
       event match {
-        case e : TestSucceeded => printf("%-20s succeced!\n",e.testName)
-        case e : TestFailed =>  printf("%-20s FAILED!\n",e.testName)
+        case e : TestSucceeded => out.printf("%-20s succeced!\n",e.testName)
+        case e : TestFailed =>  out.printf("%-20s FAILED!\n",e.testName)
         case _ => ()
       }
     }
@@ -19,19 +19,22 @@ class CustumReporter extends Reporter {
 // reports your score as a fraction between 0 and 1 for codegrade
 object ReportFraction {
   def main(args : Array[String]) : Unit  = {
-    val err = System.err
+    val out = System.out
     // prevent inventive students from printing 1.0 to
     // stderr and then getting full points
+    System.setOut(new PrintStream(new OutputStream {
+      override def write(i: Int): Unit = ()
+    }))
     System.setErr(new PrintStream(new OutputStream {
       override def write(i: Int): Unit = ()
     }))
     val scoreCounter = new ScoreCounter()
     new AllTests().runDirect(None, Args(
-      reporter = new CustumReporter(),
+      reporter = new CustumReporter(out),
       configMap = ConfigMap("scoreCounter"->  scoreCounter))
     )
-    printf("You got %d/%d points!\n", scoreCounter.points, scoreCounter.maxPoints)
-    printf("Your base grade for exercise 1 will be : %.2f\n",scoreCounter.fraction() * AllTests.MaxGrade)
-    err.printf("%.2f",scoreCounter.fraction())
+    out.printf("You got %d/%d points!\n", scoreCounter.points, scoreCounter.maxPoints)
+    out.printf("Your base grade for exercise 1 will be : %.2f\n",scoreCounter.fraction() * AllTests.MaxGrade)
+    out.printf("Fractiontouseforcodegrade %.2f",scoreCounter.fraction())
   }
 }
