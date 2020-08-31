38ff38a54fb1dcd12127598f918362c3591e0658
Atze <atzeus@gmail.com>
2020-08-31 09:00:09 +0200

    New test for mergesortedarrays

--- test/warmup/MergeSortedArraysTests.scala
+++ test/warmup/MergeSortedArraysTests.scala
@@ -6,7 +6,7 @@ import warmup.infrastructure.TestBase
 
 @RunWith(classOf[JUnitRunner])
 class MergeSortedArraysTests extends TestBase {
-    private def mergeTest(first: Array[Int], second: Array[Int], answer: Array[Int], weight: Int): Unit = {
+    private def mergeTest(first: Array[Int], second: Array[Int], answer: Array[Int]): Unit = {
         val actual = Exercises.mergeSortedArrays(first, second)
         val clue = String.format("\nInput: (%s)(%s)\nYour output: %s\n", first.mkString(" "), second.mkString(" "), actual.mkString(" "))
 
@@ -17,35 +17,40 @@ class MergeSortedArraysTests extends TestBase {
         val first = Array(1,3,5,6,10)
         val second = Array(1,4,6,8)
         val answer = Array(1,1,3,4,5,6,6,8,10)
-        val weight = 1
 
-        mergeTest(first, second, answer, weight)
+        mergeTest(first, second, answer)
     }
 
     test("merge1") {
         val first = Array(1,3,5,7,9)
         val second = Array(2,4,6,8)
         val answer = Array(1,2,3,4,5,6,7,8,9)
-        val weight = 1
 
-        mergeTest(first, second, answer, weight)
+        mergeTest(first, second, answer)
     }
 
     test("merge2") {
         val first = Array(1,2,5,8,10)
         val second = Array(2,2,6,8,9)
         val answer = Array(1,2,2,2,5,6,8,8,9,10)
-        val weight = 1
 
-        mergeTest(first, second, answer, weight)
+        mergeTest(first, second, answer)
     }
 
     test("merge3") {
         val first = Array(1,3,5,10)
         val second = Array(2,8)
         val answer = Array(1,2,3,5,8,10)
-        val weight = 1
 
-        mergeTest(first, second, answer, weight)
+        mergeTest(first, second, answer)
+    }
+
+    test(testName = "merge4") {
+        val first = Array(1,4,6,10,12)
+        val second = Array(1,2,3,5,6,7,8,9,10,13,14)
+        val answer = Array(1,1,2,3,4,5,6,6,7,8,9,10,10,12,13,14)
+
+
+        mergeTest(first, second, answer)
     }
 }
