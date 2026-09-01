8123c5adf9860d47ec85fd3822d74a4a29667267
Atze <atzeus@gmail.com>
2026-09-01 19:35:13 +0200

    New codegrade reporting

--- test/warmup/infrastructure/ReportFraction.scala
+++ test/warmup/infrastructure/ReportFraction.scala
@@ -1,7 +1,7 @@
 package warmup.infrastructure
 
 import java.io.{OutputStream, PrintStream}
-
+import java.io.{FileOutputStream, PrintWriter}
 import org.scalatest.{Args, ConfigMap, Reporter}
 import org.scalatest.events.{Event, TestFailed, TestSucceeded}
 import warmup.AllTests
@@ -20,14 +20,6 @@ class CustomReporter(val out : PrintStream) extends Reporter {
 object ReportFraction {
   def main(args : Array[String]) : Unit  = {
     val out = System.out
-    // prevent inventive students from printing 1.0 to
-    // stderr and then getting full points
-    System.setOut(new PrintStream(new OutputStream {
-      override def write(i: Int): Unit = ()
-    }))
-    System.setErr(new PrintStream(new OutputStream {
-      override def write(i: Int): Unit = ()
-    }))
     val scoreCounter = new ScoreCounter()
     new AllTests().runDirect(None, Args(
       reporter = new CustomReporter(out),
@@ -35,6 +27,21 @@ object ReportFraction {
     )
     out.printf("You got %d/%d points!\n", scoreCounter.points, scoreCounter.maxPoints)
     out.printf("Your base grade for exercise 1 will be : %.2f\n",scoreCounter.fraction() * AllTests.MaxGrade)
-    out.printf("Fractiontouseforcodegrade %.2f",scoreCounter.fraction())
+  }
+}
+
+
+// reports your score as a fraction between 0 and 1 for codegrade
+object ReportFractionCodegrade {
+  def main(args : Array[String]) : Unit  = {
+    val scoreCounter = new ScoreCounter()
+    val out = System.out
+    new AllTests().runDirect(None, Args(
+      reporter = new CustomReporter(out),
+      configMap = ConfigMap("scoreCounter"->  scoreCounter))
+    )
+    val fd3 = new PrintWriter(new FileOutputStream("/proc/self/fd/3"))
+    fd3.printf("'{ \"tag\": \"points\", \"points\": \"%f\" }",scoreCounter.fraction() * AllTests.MaxGrade)
+    fd3.close()
   }
 }
